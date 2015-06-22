630b2fb22eb81b3dfd3dd2afb2f436567f0bbd3b
manne <manne@192.168.1.2>
2015-06-22 07:30:19 -0400

    warehouseimplementation starting part

---
+++ src/warehouse/WareHouseServent.java
@@ -0,0 +1,177 @@
+package warehouse;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+import manufacturer.Manufacturer;
+import manufacturer.ManufacturerHelper;
+import manufacturer.ManufacturerPOA;
+import manufacturer.ManufacturerServent;
+
+import org.omg.CORBA.ORB;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+
+import retailer.Retailer;
+import tools.Item;
+
+public class WareHouseServent extends WarehousePOA {
+	
+	private ORB orb;
+	private InventoryManager inventoryManager;
+	private String name;
+	private HashMap<String,Manufacturer> manufactures;
+	private ArrayList<String> retailers;
+	
+	public WareHouseServent(ORB orb2,String name,  HashMap<String,Manufacturer> manufactures){
+	
+		this.orb=orb2;
+		this.name=name;
+		this.manufactures=new   HashMap<String,Manufacturer>(manufactures);
+		inventoryManager=new InventoryManager(name);
+		
+	}
+	public void connect(Manufacturer manufactureimpl,String name){
+		try{
+			
+			org.omg.CORBA.Object objRef =  orb.resolve_initial_references("NameService");
+			NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+			manufactureimpl	= ManufacturerHelper.narrow(ncRef.resolve_str(name));
+			
+		}catch (Exception e){
+			
+			System.out.println("Error:"+ e);
+			 e.printStackTrace(System.out);
+			
+		}
+		
+	}
+	//@Override
+	public Item[] getProductsByID(String productID) {
+		// TODO Auto-generated method stub
+		Item[] returnitem=null;
+		if(!(productID.isEmpty())){
+			
+			Item inventoryItem = inventoryManager.inventoryItemMap.get(productID);
+			if(inventoryItem != null){
+				
+				returnitem=add(returnitem,inventoryItem);
+				
+			}
+			
+		}
+		else{
+			
+			for(Item i:inventoryManager.inventoryItemMap.values()){
+				
+				returnitem=add(returnitem,i);
+			}
+			
+		}
+		return returnitem;
+	}
+
+	private static  Item[] add(Item[] returnitem, Item inventoryItem) {
+		// TODO Auto-generated method stub
+		returnitem=Arrays.copyOf(returnitem,returnitem.length+1);
+		returnitem[returnitem.length-1]=inventoryItem;
+		return returnitem;
+	}
+	//@Override
+	public Item[] getProductsByType(String productType) {
+		// TODO Auto-generated method stub
+		
+		Item[] returnitem=null;
+		for(Item inventoryitems:inventoryManager.inventoryItemMap.values()){
+			
+			if(inventoryitems.getProductType().equals(productType)){
+				returnitem=add(returnitem,inventoryitems);
+			}
+		}
+		return returnitem;
+	}
+
+	//@Override
+	public Item[] getProductsByRegisteredManufacturers(String manufacturerName) {
+		// TODO Auto-generated method stub
+		Item[] returnitem=null;
+		
+			if(manufactures.containsKey(manufacturerName)){
+			
+				for(Item inventoryitems:inventoryManager.inventoryItemMap.values()){
+			
+					if(inventoryitems.getManufacturerName().equals(manufacturerName)){
+						returnitem=add(returnitem,inventoryitems);
+					}
+				}
+			
+		}
+		return returnitem;
+	}
+
+	//@Override
+	public Item[] getProducts(String productID, String manufacturerName) {
+		// TODO Auto-generated method stub
+		
+		Item[] returnitem=null;
+		if(!(productID.isEmpty())){
+			
+			Item inventoryItem = inventoryManager.inventoryItemMap.get(productID);
+			if(inventoryItem != null){
+				
+				returnitem=add(returnitem,inventoryItem);
+				
+			}
+			
+		}
+		else{
+			
+			for(Item i:inventoryManager.inventoryItemMap.values()){
+				
+				returnitem=add(returnitem,i);
+			}
+			
+		}
+		return returnitem;
+	}
+
+	//@Override
+	public boolean registerRetailer(String retailerName) {
+		// TODO Auto-generated method stub
+		if(retailerName.isEmpty()){
+			
+			return false;
+			
+		}
+		else{
+			
+			retailers.add(retailerName);
+			return true;
+		
+		}
+	}
+
+	//@Override
+	public boolean unregisterRegailer(String regailerName) {
+		// TODO Auto-generated method stub
+		if(regailerName.isEmpty()){
+			
+			return false;
+		
+		}else{
+			if(retailers.contains(regailerName)){
+
+				retailers.remove(regailerName);
+				return true;
+				
+			}else{
+				
+				return false;
+			}
+
+			
+		}
+	
+	}
+}
---
+++ src/warehouse/WareHouseServer.java
@@ -0,0 +1,51 @@
+package warehouse;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.omg.CORBA.ORB;
+import org.omg.CosNaming.NameComponent;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+import org.omg.PortableServer.POA;
+import org.omg.PortableServer.POAHelper;
+
+import manufacturer.*;
+import retailer.*;
+
+public class WareHouseServer {
+	String name;
+	private HashMap<String,Manufacturer> maufactures;
+	InventoryManager inventorymanager;
+	public WareHouseServer(){
+		
+	}
+	
+	public void main(String []args){
+		
+		
+		try{
+			
+			ORB orb=ORB.init(args,null);
+			POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+			rootpoa.the_POAManager().activate();
+			WareHouseServent warehouseimpl=new WareHouseServent(orb, name, maufactures);
+			org.omg.CORBA.Object ref = rootpoa.servant_to_reference(warehouseimpl);
+			Warehouse wref=WarehouseHelper.narrow(ref);
+			org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
+			NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+			String wname = name;
+		    NameComponent path[] = ncRef.to_name( wname );
+		    ncRef.rebind(path, wref);
+			System.out.println("warehouse server started and waiting....");
+			orb.run();
+	     
+		}catch(Exception e){
+			
+			System.out.println("Error:"+ e);
+			 e.printStackTrace(System.out);
+			
+		}
+		System.out.println("warehouse server exiting....");
+	}
+
+}
