ef1a026f8ab9eff96fb9603c1f70c8ecbbbaf77c
manne <manne@192.168.1.2>
2015-06-22 07:33:29 -0400

    added manufacturer and inventory files but not implemented

---
+++ src/warehouse/InventoryManager.java
@@ -0,0 +1,11 @@
+package warehouse;
+import java.util.HashMap;
+
+import tools.*;
+public class InventoryManager {
+	public HashMap<String, Item> inventoryItemMap;
+	private String warehouseName;
+	public InventoryManager(String name){
+		
+	}
+}
